c234198bd3be654cfe1f7c5f8bbe5394b2776e37
Marc Hauschildt <marc.hauschildt@gmail.com>
2018-09-06 15:25:48 -0500

    Header with search bar

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('works');
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+});
+app.listen(PORT, () => {
+    console.log(`Server is listening on port ${PORT}`);
+} );

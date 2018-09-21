4fae12290655cea5145ae0f1ff938830d0dedcc7
Marc Hauschildt <marc.hauschildt@gmail.com>
2018-09-21 15:28:57 -0500

    Created product list

---
+++ js/products.js
@@ -0,0 +1,16 @@
+var products = [
+
+    {
+        title: "Fight For Iowa Hawkeye Tee",
+        price: 18.50,
+        sizes: ["S", "M", "L", "XL", "XXL"],
+        description: "This Iowa Hawkeye Champion brand tee features \"Fight for Iowa\" written on the front",
+        colors: ["Black", "White", "Gold"],
+        materials: ["80% Cotton", "20% Polyester"],
+        brand: "Champion",
+        categories: ["men", "tee", "short-sleeve"],
+        product_id: "C1900"
+    },
+    
+    
+];
